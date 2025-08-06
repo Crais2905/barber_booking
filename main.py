2e392c0e7db49ff3e82edb3fa29e953a5258fc91
Crais2905 <slisovets2905@gmail.com>
2025-08-06 15:44:57 +0300

    create db and session

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from db.session import engine
+from db.models import Base
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
